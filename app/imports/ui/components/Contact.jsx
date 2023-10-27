aa4b0d08416c4549a494ddca2da914463a52f03b
Tiffany Ngo <ngotiff15@gmail.com>
2023-10-26 23:59:59 -1000

    list contacts page mockup finished in 15:00 minutes.

---
+++ app/imports/ui/components/Contact.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image } from 'react-bootstrap';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const Contact = ({ contact }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Image src={contact.image} width={75} />
+    </Card.Header>
+    <Card.Title>
+      {contact.firstName} {contact.lastName}
+    </Card.Title>
+    <Card.Subtitle>
+      {contact.address}
+    </Card.Subtitle>
+    <Card.Body>
+      <Card.Text>
+        {contact.description}
+      </Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+Contact.propTypes = {
+  contact: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    address: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
+};
+
+export default Contact;
