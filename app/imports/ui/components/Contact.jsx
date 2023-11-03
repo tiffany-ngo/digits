7b50fddc121461efdf79b76df32c23318914a427
tiffany-ngo <ngotiff15@gmail.com>
2023-11-02 21:15:56 -1000

    contact forms finished in 31:03 minutes.

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const Contact = ({ contact }) => (
@@ -18,6 +19,7 @@ const Contact = ({ contact }) => (
       <Card.Text>
         {contact.description}
       </Card.Text>
+      <Link to={`/edit/${contact._id}`}>Edit</Link>
     </Card.Body>
   </Card>
 );
@@ -30,6 +32,7 @@ Contact.propTypes = {
     address: PropTypes.string,
     image: PropTypes.string,
     description: PropTypes.string,
+    _id: PropTypes.string,
   }).isRequired,
 };
 
